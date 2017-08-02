079f271c6a926bfca5ded0862a6060b0ce852fd9
Jeff Hays <jhays@moguldom.com>
2017-08-02 14:25:08 -0400

    Updating some commands and things. Begin reworking of global bot arrays for seen, tell, etc.

--- bot.js
+++ bot.js
@@ -2,20 +2,21 @@
 var config = require( './config' );
 var irc = require( 'irc' );
 var nickserv = require( 'nickserv' );
-var google = require( 'google' );
+var google_search = require( 'google-search' );
 var request = require( 'request' );
 var jsdom = require( 'jsdom' );
 var moment = require( 'moment-timezone' );
 
 // Initialize global bot variables
-google.resultsPerPage = 1;
+// google.resultsPerPage = 1;
 var tell = [];
 var seen = [];
+var watch = [];
 
 // Create the bot
 var bot = new irc.Client( config.server, config.name, {
 	// channels: config.channels,
-	localAddress: config.localAddress,
+	// localAddress: config.localAddress,
 	realName: config.realName,
 	autoRejoin: true
 });
@@ -27,6 +28,12 @@ var ns = new nickserv( config.name, {
 });
 ns.attach( 'irc', bot );
 
+// Instantiate Google search API
+var google = new google_search({
+	key: config.google_api_key,
+	cx: config.google_cx
+});
+
 // Export
 module.exports = {
 	config: config,
@@ -34,8 +41,49 @@ module.exports = {
 	nickserv: ns,
 	moment: moment,
 	request: request,
-	google: google,
+	google: false,
 	dom: jsdom,
+	search: function( sites, chan, who, from, str ) {
+		// var scrape = new scraper({
+		// 	keyword: str,
+		// 	language: 'us',
+		// 	tld: 'us',
+		// 	results: 100
+		// });
+		// scrape.getGoogleLinks.then(function( links ) {
+		// 	return links;
+		// }).catch(function( err ) {
+		// 	if ( config.debug ) console.error( err );
+		// });
+
+		var prefix = who ? who + ': ' : from + ': ';
+		var options = {
+			q: str,
+			start: 1,
+			fileType: '',
+			gl: 'us',
+			lr: 'lang_en', 
+			context: '',
+			num: 10
+		};
+		if ( sites != '' ) {
+			sites = typeof sites == 'object' ? sites.join(':') : sites;
+			options.siteSearch = sites;
+		} else {
+			options.siteSearch = '';
+		}
+		var url = google._generateUrl( options );
+		if ( typeof url == 'object' ) {
+			if ( config.debug ) console.log( 'The URL is: ' + url.href );
+			request( url.href, function( error, response, body ) {
+				if ( error && config.debug ) console.error( error );
+				var search = JSON.parse( body );
+				if ( typeof search == 'object' && search.items.length ) {
+					bot.say( chan, prefix + search.items[0].link );
+				}
+			});
+		}
+	},
 	seen: {
 		list: function() {
 			return seen;
@@ -43,27 +91,32 @@ module.exports = {
 		add: function( nick ) {
 			return seen.push( nick );
 		},
-		search: function( nick ) {
+		search: function( nick, chan, who, from ) {
 			var seenchans = [];
+
+			if ( config.debug ) {
+				console.log( '[Seen search] for: ' + nick );
+			}
 
 			// Check channels first
 			for ( var chan in bot.chans ) {
 				// Normalize the case of each user for case insensitive checking
-				console.log( 'CHAN:' );
-				console.log( chan );
+				if ( config.debug ) console.log( 'Checking chan: ' + chan );
 				for ( var user in bot.chans[ chan ].users ) {
-					if ( user.toLowerCase() == nick ) {
+					if ( user.toLowerCase() == nick.toLowerCase() ) {
+						if ( config.debug ) console.log( 'Found ' + nick + ' in ' + chan );
 						seenchans.push( chan );
 					}
 				}
 			}
 
 			if ( seenchans.length ) {
-				return nick + ' is currently in: ' + seenchans.join( ', ' );
+				bot.say( chan, nick + ' is currently in: ' + seenchans.join( ', ' ) );
+				return true;
 			} else {
 				// Search through seen array
 				seen.forEach( function( value, index, array ) {
-					if ( value.nick == str ) {
+					if ( value.nick == nick ) {
 						// Setup the seen message
 						var msg = 'Last seen ' + value.nick + ' ';
 						var time = ' on ' + value.time;
@@ -82,7 +135,11 @@ module.exports = {
 						return msg;
 					}
 				});
+				bot.say( chan, msg );
+				return true;
 			}
+			var msg = who ? who + ': ' + 'I haven\'t seen ' + nick : from + ': I haven\'t seen ' + nick;
+			bot.say( chan, msg );
 			return false;
 		},
 		remove: function( nick ) {
@@ -105,7 +162,7 @@ module.exports = {
 				from: from,
 				to: to,
 				message: message,
-				date: moment().tz( 'America/New_York' ).format( 'M/DD/YY h:mm:ssa z' )
+				date: moment().tz( 'America/New_York' ).format( 'MMMM Do YYYY, h:mm:ss a z' )
 			});
 		},
 		check: function( who, channel ) {
@@ -124,7 +181,7 @@ module.exports = {
 			tell.forEach( function( value, index, array ) {
 				if ( value.to == who ) {
 					if ( config.debug ) console.log( 'Delivering .tell message #' + index + ' to ' + who );
-					var date = moment( value.date ).tz( 'America/New_York' ).format( 'M/DD/YY h:mm:ssa z' );
+					var date = moment( value.date ).tz( 'America/New_York' ).format( 'MMMM Do YYYY, h:mm:ss a z' );
 					msg = '[' + value.from + ' ' + date + ']: ' + value.message;
 					inbox.push( msg );
 					told.push( index );
@@ -157,6 +214,71 @@ module.exports = {
 					tell.splice( value, 1 );
 				});
 				return true;
+			}
+			return false;
+		}
+	},
+	watch: {
+		add: function( url, selector, value, interval, type, filter, channel ) {
+			watch.push({
+				url: url,
+				selector: selector,
+				value: value,
+				interval: interval,
+				type: 'text',
+				filter: filter,
+				channel: channel,
+				result: false
+			});
+		},
+		remove: function( url, selector, value ) {
+			for ( var item in watch ) {
+				if ( item.url == url && item.selector == selector && item.value == value ) {
+					watch.splice( item, 1 );
+				}
+			}
+		},
+		check: function( interval ) {
+			var i = 0;
+			for ( var item in watch ) {
+				if ( item.interval == interval ) {
+					if ( config.debug ) {
+						console.log( 'checking watched item: ', item );
+					}
+					// Scrape the site for contents from selector
+					request( item.url, function( error, response, body ) {
+						if ( ! error ) {
+							// Load bot.dom for DOM parsing
+							jsdom.env(
+								body,
+								[ 'http://code.jquery.com/jquery-2.2.3.min.js' ],
+								function( err, window ) {
+									// Get remote content
+									if ( err ) console.log( 'jsdom err: ', err );
+									var $ = window.jQuery;
+									console.log( 'body:', $('body').html() );
+									switch ( item.type ) {
+										case 'text':
+											var result = $( item.selector ).text();
+											break;
+										case 'html':
+											var result = $( item.selector ).html();
+											break;
+									}
+
+									// Parse remote content
+									if ( result != item.result ) {
+										// Update watch list
+										bot.say( message.args[0], who ? who + ': ' + msg : msg );
+									}
+								}
+							);
+						} else {
+							console.log( error );
+						}
+					});
+				}
+				i++;
 			}
 			return false;
 		}
---
+++ events/cron.js
@@ -0,0 +1,12 @@
+/*
+ *	This file handles 'message' events from the irc client
+ */
+
+// Includes
+var bot = require( '../bot' );
+
+// Every 1 minute
+// var minutes = 1, the_interval = minutes * 60 * 1000;
+// setInterval( function() {
+	
+// }, the_interval );
